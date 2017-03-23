acba819911dc4b0c740159830892e62e1856458f
Ben Metzger and Peter Karellas <peter@>
2017-03-23 16:33:00 -0700

    add random result

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
-$(document).ready(
-  function() {
+$(document).ready(function() {
     $("#info-form").submit(function(event) {
       event.preventDefault();
       var range = $("#range").val();
       var magic = $("#magic").val();
       var jewelry = $("#jewelry").val();
 
-      if (
-      };
+      var ran = Math.random()
 
+      if (ran < .3) {
+        alert("gandalf")
+      } else if (ran > .3 && ran < .6) {
+        alert("frodo")
+      } else if (ran > .6) {
+        alert("legolas")
+      }
     });
-  }
-);
+  });
