3405b80948b2285f0a02c28f00da89322fc2980d
Ben Metzger and Peter Karellas <peter@>
2017-03-23 16:00:15 -0700

    add 3 questions

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,12 @@ $(document).ready(
   function() {
     $("#info-form").submit(function(event) {
       event.preventDefault();
-      var dob = $("#born").val();
-      var pet = $("input:radio[name=pet]:checked").val();
-      var sign = $("#sign").val();
+      var range = $("#range").val();
+      var magic = $("#magic").val();
+      var jewelry = $("#jewelry").val();
 
-      if (pet === "cats" && sign === "Aries" || sign === "Leo" || sign === "Taurus") {
-        $("h2").text("Madonna");
+      if (
       };
-
-      if (pet === "dogs" && sign === "Aries" || sign === "Taurus") {
-        $("h2").text("Tom Cruise");
-      };
-
-      if (pet === "dogs" && sign === "Leo") {
-        $("h2").text("Brad Pitt");
-      };
-
-      if (pet === "nopets") {
-        $("h2").text("Donald Trump");
-      };
-
 
     });
   }
